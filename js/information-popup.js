e51743dcf6897098e22794c8f4b90211cdd65c55
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-12-19 12:00:47 +0530

    added js

--- js/information-popup.js
+++ js/information-popup.js
@@ -1,28 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-      document.getElementById("overlay").style.display = "flex";
-  
-      // Close the popup when the user clicks anywhere on the overlay
-      // document
-      //   .getElementById("overlay")
-      //   .addEventListener("click", function (event) {
-      //     if (event.target.id === "overlay") {
-      //       closePopup();
-      //     }
-      //   });
-  
-      // Prevent the popup from closing when the user clicks inside the form
-    }, 3000); // Display the popup after 3 seconds
-  });
-  
-  document.addEventListener("click", function (event) {
-    const overlay = document.getElementById("overlay");
-    if (overlay.style.display === "flex" && !overlay.contains(event.target)) {
-      closePopup();
-    }
-  });
-  
-  function closePopup() {
-    document.getElementById("overlay").style.display = "none";
+  // Check if the user has accepted cookies before
+  if (!localStorage.getItem("cookiesAccepted")) {
+    // Show the cookie dialog if not accepted
+    showCookieDialog();
   }
-  
+
+  // Add click event listener to the "Accept Cookies" button
+  document
+    .getElementById("acceptCookiesBtn")
+    .addEventListener("click", function () {
+      // Hide the cookie dialog
+      hideCookieDialog();
+
+      // Set a flag in localStorage to remember that the user has accepted cookies
+      localStorage.setItem("cookiesAccepted", true);
+    });
+});
+
+function showCookieDialog() {
+  document.getElementById("cookieDialog").style.display = "block";
+}
+
+function hideCookieDialog() {
+  document.getElementById("cookieDialog").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("overlay").style.display = "flex";
+  }, 3000); // Display the popup after 3 seconds
+});
+
+document.getElementById("overlay").addEventListener("click", function (event) {
+  const popup = document.getElementById("popup");
+  // Check if the click target is not the overlay or its descendants
+  if (!popup.contains(event.target)) {
+    closePopup();
+  }
+});
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+}
