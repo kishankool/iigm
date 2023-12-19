a1057fdba802173e6496f62973583b38d03a5a69
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-12-19 11:38:42 +0530

    removed comments

--- js/information-popup.js
+++ js/information-popup.js
@@ -1,44 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   setTimeout(function () {
-//     document.getElementById("overlay").style.display = "flex";
-
-//     // Close the popup when the user clicks anywhere on the overlay
-//     // document
-//     //   .getElementById("overlay")
-//     //   .addEventListener("click", function (event) {
-//     //     if (event.target.id === "overlay") {
-//     //       closePopup();
-//     //     }
-//     //   });
-
-//     // Prevent the popup from closing when the user clicks inside the form
-//   }, 3000); // Display the popup after 3 seconds
-// });
-
-// document.addEventListener("click", function () {
-//   const check = document.getElementById("overlay").style.display;
-//   console.log('check',check);
-//   if (document.getElementById("overlay").style.display === "flex") {
-//     document.getElementById("overlay").style.display = "none";
-//   }
-// });
-
-// function closePopup() {
-//   document.getElementById("overlay").style.display = "none";
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
       document.getElementById("overlay").style.display = "flex";
   
       // Close the popup when the user clicks anywhere on the overlay
-      document
-        .getElementById("overlay")
-        .addEventListener("click", function (event) {
-          if (event.target.id === "overlay") {
-            closePopup();
-          }
-        });
+      // document
+      //   .getElementById("overlay")
+      //   .addEventListener("click", function (event) {
+      //     if (event.target.id === "overlay") {
+      //       closePopup();
+      //     }
+      //   });
   
       // Prevent the popup from closing when the user clicks inside the form
     }, 3000); // Display the popup after 3 seconds
