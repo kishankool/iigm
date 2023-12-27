ed08708ce9c8b125adae9f4472d6d02cb66f2b2c
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-12-27 11:55:47 +0530

    Venktesh title (#20)
    * title-changed
    
    * change-in-apply-button
    
    * demo-video-added
    
    * new-google-form-added
    
    * change-in-detail-page
    
    * social-media-icon
    
    * hindi-pdf-added
    
    * link-change
    
    * link_added
    
    * payment-pop-up
    
    * added payment
    
    * made div stylish
    
    * added payment data
    
    * made div repsonsive
    
    ---------
    
    Co-authored-by: Venktesh123 <8303531530venktesh@gmail.com>

---
+++ js/acconut-detail.js
@@ -0,0 +1,9 @@
+function openPopup() {
+  document.getElementById('popup-Container').style.display = 'block';
+  document.getElementById('overlay-2').style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('popup-Container').style.display = 'none';
+  document.getElementById('overlay-2').style.display = 'none';
+}
